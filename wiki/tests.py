56f72da516dad2078b818c25c8926a8ad305fe2a
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-07-17 06:06:57 -0700

    added edit

--- wiki/tests.py
+++ wiki/tests.py
@@ -6,12 +6,19 @@ from wiki.models import Article
 class Wiki(TestCase):
     def test_details_page(self):
         user = User.objects.create()
-        Article.objects.create(title="cs is cool", content="Content", author=user)
+        article = Article.objects.create(title="cs is cool", content="Content", author=user)
 
         response = self.client.get('/w/cs-is-cool', {}, True)
-        # self.assertContains(response.context, Article)
+        self.assertContains(response, "cs is cool")
         self.assertEqual(response.status_code, 200)
 
-    #
-    # def test_details_page_edit(self):
-    #     article = Article.objects.create(title="cs is cool", content="Content", author=user)
+
+    def test_details_page_edit(self):
+        article = Article.objects.create()
+        article.save()
+
+        edit_post = self.client.post('/edit/',
+        {
+            'title': 'edit title',
+            'content': 'content is cool',
+        })
--- wiki/views.py
+++ wiki/views.py
@@ -27,10 +27,17 @@ class ArticleDetailView(DetailView):
 
     def get(self, request, slug):
         """ Returns a specific wiki article by slug. """
+
         article = self.get_queryset().get(slug__iexact=slug)
-        return render(request, 'article.html', {
-          'article': article
-        })
+        context = {'form': ArticleForm(), 'article': article}
+        return render(request, 'article.html', context)
+
+    def post(self, request, *args, **kwargs):
+      form = ArticleForm(request.POST)
+      if form.is_valid():
+          article = form.save()
+          return HttpResponseRedirect(reverse_lazy('wiki-details-page', args=[article.slug]))
+      return render(request, 'article.html', {'form': form})
 
 class ArticleCreateView(CreateView):
 
